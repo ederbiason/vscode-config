5db4d67a5b04547f22b7ec20b0ecc9faafc857a5
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2023-02-20 18:25:05 -0300

    chore: Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,17 @@
+import { Github } from "lucide-react";
+
 export function Footer() {
     return (
-        <div>
-            
+        <div className="text-sm px-3 text-[#8F8CA8] flex gap-3 items-center justify-end bg-[#2a273f]">
+            <a
+                href="https://github.com/ederbiason/vscode-config"
+                target="_blank"
+                rel="noreferrer"
+                className="flex gap-1 items-center text-xs hover:text-[#E0DEF2]"
+            >
+                <Github size={12} />
+                Github
+            </a>
         </div>
     )
 }
