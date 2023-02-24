e4a0ce1f6cf87ad48ab3e3f280bb55169c02f25f
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2023-02-24 15:20:56 -0300

    chore: fetching data from notion page

--- src/lib/notion.ts
+++ src/lib/notion.ts
@@ -1,13 +1,24 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-
-const { Client } = require('@notionhq/client');
+import { Client, isFullBlock } from '@notionhq/client';
+import { CodeBlockObjectResponse } from '@notionhq/client/build/src/api-endpoints';
 
 const notion = new Client({
     auth: process.env.NOTION_API_KEY
 })
 
-export default async function handler(databaseId: string) {
-    const database = await notion.databases.query({
-        database_id: databaseId
+export default async function getNotionPageContent(pageId: string) {
+    const { results } = await notion.blocks.children.list({
+        block_id: pageId
     })
+
+    const codeBlock = results.find(block => isFullBlock(block) && block.type === "code") as CodeBlockObjectResponse | undefined
+
+    console.log(results)
+
+    if(!codeBlock) {
+        throw new Error(`Failed to fetch Notion content of ID: ${pageId}`)
+    }
+
+    const { plain_text } = codeBlock.code.rich_text[0]
+
+    return { content: plain_text }
 }
