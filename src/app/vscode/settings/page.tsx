f15fa37cdc61f6618425838903f2f644c1fcf018
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2023-02-27 15:30:00 -0300

    chore:  new settings/about page

--- src/app/vscode/settings/page.tsx
+++ src/app/vscode/settings/page.tsx
@@ -1,7 +1,20 @@
+import { CodePreview } from '@/components/CodePreview'
+import { getNotionPageContent } from '@/lib/notion'
+import shiki from 'shiki'
+
+export const metadata = {
+    title: 'About me'
+}
+
 export default async function Settings() {
-    return (
-        <div>
-            
-        </div>
-    )
+    const pageId = "b8082d0ba4414657b4ed4815ff318284"
+    const { content } = await getNotionPageContent(pageId)
+
+    const highlighter = await shiki.getHighlighter({
+        theme: 'rose-pine-moon'
+    })
+
+    const code = highlighter.codeToHtml(content, { lang: 'md' })
+
+    return <CodePreview code={code} />
 }
