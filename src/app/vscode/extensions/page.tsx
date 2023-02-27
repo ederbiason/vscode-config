fbc0ab0427bdc7126d2a9d33cd60da8e3a82028d
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2023-02-27 15:43:09 -0300

    chore: new stacks/skills page

--- src/app/vscode/extensions/page.tsx
+++ src/app/vscode/extensions/page.tsx
@@ -1,3 +1,20 @@
+import { CodePreview } from '@/components/CodePreview'
+import { getNotionPageContent } from '@/lib/notion'
+import shiki from 'shiki'
+
+export const metadata = {
+    title: 'Stacks and Skills'
+}
+
 export default async function Extensions() {
-    
+    const pageId = "db86e0e1ad7f4c209f9a8930e9c79e76"
+    const { content } = await getNotionPageContent(pageId)
+
+    const highlighter = await shiki.getHighlighter({
+        theme: 'rose-pine-moon'
+    })
+
+    const code = highlighter.codeToHtml(content, { lang: 'md' })
+
+    return <CodePreview code={code} />
 }
